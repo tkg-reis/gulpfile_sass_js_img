47b9711e24530429a573be839b6d3020349bef78
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2023-05-16 18:32:09 +0900

    all add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // ----- 360px以下のスマートフォンサイズ対応 -----
+
 const viewport = document.querySelector('meta[name="viewport"]');
 function switchViewport() {
     const value =
@@ -13,6 +14,7 @@ function switchViewport() {
 }
 addEventListener('resize', switchViewport, false);
 switchViewport();
+
 // console.log(viewport.getAttribute('content'));
 
 //---------------------------------------------------
@@ -21,49 +23,151 @@ switchViewport();
 
 // header toggle animation
 
-// const toggleWrapper = document.querySelector('.header_container__toggle');
+{
+  const modal = document.querySelector(".header_toggle");
+  const toggle = document.querySelector(".header_toggle__bars");
+  const toggleItems = document.querySelectorAll(".header_toggle_link");
+  // フラグ変数
+  let done = false;
 
-// const respMenu = document.querySelector('.header_container__respMenu');
-
-let done = true;
-toggleWrapper.addEventListener("click",() => {
-    respMenu.classList.add('opened');
+  toggle.addEventListener("click", () => {
+    modal.style.display = "block";
+    toggle.classList.add("open");
     setTimeout(() => {
+      done = true;
+    }, 10);
+  });
+  toggle.addEventListener("click", () => {
+    if (done === true) {
+      modal.style.display = "none";
+      toggle.classList.remove("open");
+      setTimeout(() => {
         done = false;
-    },100)
-});
-document.addEventListener('click',() => {
-    if(done === false) {
-        respMenu.classList.remove('opened');
-        setTimeout(() => {
-            done = true;
-        },100);
+      }, 10);
     }
+  });
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+      toggle.classList.remove("open");
+    }
+  });
+  toggleItems.forEach((toggleItem) => {
+    toggleItem.addEventListener("click", () => {
+      modal.style.display = "none";
+      toggle.classList.remove("open");
+    });
+  });
+}
+
+// swiper
+
+try {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        effect: "fade",
+        autoplay: true,
+    });
+} catch (error) {
+    console.log(error.message);
+}
+
+// gsap
+
+let workBox = document.querySelectorAll('.gsap_box');
+let workImg = document.querySelectorAll('.gsap_module');
+let gsapToArray = gsap.utils.toArray(workBox);
+let gsapToImgArray = gsap.utils.toArray(workImg);
+
+// 関連(同一クラス名に)関数を一緒に扱う
+
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+  trigger: gsapToImgArray[0],
+  start: 'top center',
+  end: 'bottom center',
+  scrub: -1,
+  toggleActions: "restart none reverse none",
+  onEnter: () => {
+    gsap.to(gsapToArray[0], {
+      scale: 1.5,
+      opacity: 1,
+      duration: 1,
+    });
+  },
+  onLeave: () => {
+    gsap.to(gsapToArray[0], {
+        scale: 1,
+        opacity: .5,
+    });
+  }
 });
 
-// header hide and exist animation
-
-let width = window.innerWidth;
-
-if(width < 768) {
-    const header = document.querySelector('.header');
-    let scrollPoint = 0; 
-    let lastPoint = 0; 
-    
-    window.addEventListener("scroll",function(){
-        
-        scrollPoint = window.scrollY;
-        
-        if(scrollPoint > lastPoint){ 
-            header.classList.add('header_hide');
-        }else{ 
-            header.classList.remove('header_hide');
-        }
-
-        lastPoint = scrollPoint;
-        // ここでリセット
+ScrollTrigger.create({
+  trigger: gsapToImgArray[1],
+  start: 'top center',
+  end: 'bottom center',
+  scrub: -1,
+  toggleActions: "restart none reverse none",
+  onEnter: () => {
+  gsap.to(gsapToArray[1], {
+    scale: 1.5,
+    opacity: 1,
+    duration: 1,
+  });
+  },
+  onLeave: () => {
+    gsap.to(gsapToArray[1], {
+      scale: 1,
+      opacity: .5,
     });
-}
+  }
+});
+
+ScrollTrigger.create({
+    trigger: gsapToImgArray[2],
+    start: 'top center',
+    end: 'bottom center',
+    scrub: -1,
+    toggleActions: "restart none reverse none",
+  onEnter: () => {
+    gsap.to(gsapToArray[2], {
+      scale: 1.5,
+      opacity: 1,
+      duration: 1,
+    });
+  },
+  onLeave: () => {
+    gsap.to(gsapToArray[2], {
+      scale: 1,
+      opacity: .5,
+    });
+  }
+});
+// gsapToArray.forEach((el, index) => {
+//     console.log(el);
+//     ScrollTrigger.create({
+//         trigger:  index === 0 ? el : "",
+//         start: 'top 10%',
+//         endTrigger: index === 1 ? el : "",
+//         toggleClass: { targets:  index === 0 ? el : "", className: "active"},
+//         onToggle: () => {
+            
+//             ScrollTrigger.create({
+//                 trigger:  index === 1 ? el : "",
+//                 start: 'center center',
+//                 toggleClass: {targets:  index === 0 ? el : "", className: "active"},
+//             });
+//         } 
+//     });
+//     ScrollTrigger.create({
+//         trigger:  index === 2 ? el : "",
+//         start: 'top 0%',
+//         end : 'bottom 90%',
+//         toggleClass: {targets:  index === 0 ? el : "", className: "active"},
+//     });
+// });
 
 // page top animation
 
